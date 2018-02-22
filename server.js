5773b63bc42f2379e7f8ab57e52a63fb9050f1d6
saikumarm80 <36702728+saikumarm80@users.noreply.github.com>
2018-02-22 21:58:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/test3', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'test3.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'test2.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
